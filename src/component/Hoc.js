ae151c748b4fb483da9392819dd364089ab2dd1e
syedalihussain100 <smuheminali@gmail.com>
2021-11-01 12:47:09 +0500

    changes

--- src/component/Hoc.js
+++ src/component/Hoc.js
@@ -3,14 +3,20 @@ import React, { useState } from 'react'
 function Hoc() {
     return (
         <div>
-        <MyHoc/>
-        <MyHoc/>
-        <MyHoc/>
+            <HocRed cmd={MyHoc} />
+            <HocGreen cmd={MyHoc} />
         </div>
     )
 }
 
 
+function HocRed(props) {
+    return <h2 style={{ color: 'red' }}><props.cmd /></h2>
+}
+
+function HocGreen(props) {
+    return <h2 style={{ color: 'green' }}><props.cmd /></h2>
+}
 
 
 function MyHoc() {
@@ -18,9 +24,9 @@ function MyHoc() {
     return (
         <div>
             <h1>Counting:{count}</h1>
-            <button onClick={() => setCount(count+1)}>Increament ++</button>
+            <button onClick={() => setCount(count + 1)}>Increament ++</button>
         </div>
     )
 }
 
-export default Hoc
+export default Hoc;
