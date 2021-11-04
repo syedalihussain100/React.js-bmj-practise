ee71b06d52badffcf14ad9c40e44f84bc7bfa253
syedalihussain100 <smuheminali@gmail.com>
2021-11-04 21:40:39 +0500

    changes

--- src/Screen/Services.js
+++ src/Screen/Services.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Services() {
+    const [person, setPerson] = useState({ firstname: '', lastname: '', email: '' });
+    const [people, setPeople] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (person.firstname && person.lastname && person.email) {
+            const newPerson = { ...person, id: new Date().getTime().toString() };
+            setPeople({ ...people, newPerson });
+           console.log(newPerson)
+        } else {
+            alert('Please Submit The Form!')
+        }
+    }
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setPerson({ ...person, [name]: value });
+    }
     return (
         <div>
-        <h2>hello World</h2>            
+            <h2>React.Js Form State</h2>
+            <br />
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='Enter Your First Name' name='firstname'
+                    value={person.firstname} onChange={handleChange}
+                />
+                <br /> <br />
+                <input type="text" placeholder='Enter Your Last Name' name='lastname'
+                    value={person.lastname} onChange={handleChange}
+                />
+                <br /> <br />
+                <input type="email" placeholder='Ente Your Email' name='email'
+                    value={person.email} onChange={handleChange}
+                />
+                <button type='submit'>Form Submit</button>
+            </form>
         </div>
     )
 }
